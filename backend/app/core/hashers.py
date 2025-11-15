ff9633b9110e734e93885ca1c7af4a49de2feec7
I'm Ademir <falcaojr.ademir@gmail.com>
2025-11-15 17:36:41 -0300

    cadastro e listagem de usuarios

--- backend/app/core/hashers.py
+++ backend/app/core/hashers.py
@@ -1,8 +1,16 @@
 import bcrypt
-
 from .interfaces import IPasswordHasher
 
 
 class BcryptHasher(IPasswordHasher):
+    def hash(self, password: str) -> str:
+
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+        return hashed.decode("utf-8")
+    
     def verify(self, plain_password: str, hashed_password: str) -> bool:
-        return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+        return bcrypt.checkpw(
+            plain_password.encode("utf-8"), 
+            hashed_password.encode("utf-8")
+        )

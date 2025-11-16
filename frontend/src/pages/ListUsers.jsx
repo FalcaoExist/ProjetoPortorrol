5611388de1c415002e2b666ce4bca01b98887752
mariahuth <mariap.fhuth@gmail.com>
2025-11-16 18:56:55 -0300

    Adicionar modal de adicionar comprador. Sem integração com o back!

--- frontend/src/pages/ListUsers.jsx
+++ frontend/src/pages/ListUsers.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import Header from "../components/header/Header"
 import Navbar from "../components/nav_bar/NavBar"
 import UserProfileSummary from "../components/user_profile_summary/UserProfileSumary"
 import UsersTable from "../components/users_table/UsersTable"
+import AddBuyerModal from "../components/add_buyer_modal/AddBuyerModal.jsx";
 
 export default function ListUsers(){
+    const [openModal, setOpenModal] = useState(false);
+
     return (
-    <div className="grid min-h-screen grid-cols-[16rem_minmax(0,1fr)]">
+        <div className="grid min-h-screen grid-cols-[16rem_minmax(0,1fr)]">
             <Navbar />
 
             <main className="min-w-0">
@@ -15,10 +19,16 @@ export default function ListUsers(){
                     <UserProfileSummary role="Gestor" userName={"Dionatas Terres"} userEmail={"dionatas.terres@portorrol.com"}/>
                     <section className="xl:pr-48 pl-20 md:px-20">
                         <UsersTable users={[]}/>
-                        <button to="" className="bg-[#5A44B0] hover:bg-white text-white hover:text-black shadow-lg font-poppins uppercase text-sm p-2 rounded-md mt-6"> Adicionar Comprador</button>
+                        <button
+                            onClick={() => setOpenModal(true)}
+                            className="bg-[#5A44B0] hover:bg-white text-white hover:text-black shadow-lg font-poppins uppercase text-sm p-2 rounded-md mt-6"
+                        >
+                            Adicionar Comprador
+                        </button>
                     </section>
                 </div>
             </main>
+            <AddBuyerModal isOpen={openModal} onClose={() => setOpenModal(false)} />
         </div>
     )
 }

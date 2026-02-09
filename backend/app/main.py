b05db64741565d04300d12c3ec748f1ac2264a2a
eduardo-souza-1 <eduardonunesneveesss@gmail.com>
2026-02-09 20:49:55 -0300

    Modify CORS settings for Vercel subdomains
    Updated CORS middleware to allow origins from Vercel subdomains.

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,12 +9,12 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://portorrol.vercel.app",
+    
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origin_regex=r"https://portorrol.*\.vercel\.app", 
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],

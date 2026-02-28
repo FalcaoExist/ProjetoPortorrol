f0f594faad4d37df4f25197d2c2a1984fc1435a9
RaissonGitHub <raissonarbustepaludo@gmail.com>
2026-02-28 01:22:31 -0300

    Retirando saving

--- frontend/src/components/charts/LeadtimeSavingCard.jsx
+++ frontend/src/components/charts/LeadtimeSavingCard.jsx
@@ -4,10 +4,10 @@ export default function LeadtimeSavingCard({ leadtime = null, saving = 8 }) {
   const displayLeadtime = (leadtime === null || leadtime === undefined) ? '—' : `${leadtime}`;
   return (
     <div className="border border-[#e5e5e5] rounded-xl p-4 flex flex-col items-center justify-center shadow-sm bg-white w-full min-w-0 h-full">
-      <span className="text-sm md:text-base text-[#464255] font-poppins">Leadtime médio</span>
-      <span className="text-xl md:text-2xl font-bold text-[#2d253b] font-poppins leading-tight">{displayLeadtime} {leadtime === null ? '' : 'dias'}</span>
-      <span className="text-sm text-[#464255] font-poppins mt-1">Saving</span>
-      <span className="text-2xl  font-bold text-[#2d253b] font-poppins">{saving}%</span>
+      <span className="text-md md:text-lg text-[#464255] font-poppins">Leadtime médio</span>
+      <span className="text-xl md:text-3xl font-bold text-[#2d253b] font-poppins leading-tight">{displayLeadtime} {leadtime === null ? '' : 'dias'}</span>
+      {/* <span className="text-sm text-[#464255] font-poppins mt-1">Saving</span>
+      <span className="text-2xl  font-bold text-[#2d253b] font-poppins">{saving}%</span> */}
     </div>
   );
 }
